68b624575b122a4ca2b6ce94d5573eb07669f2b4
cloudluis22 <cloudluis22@hotmail.com>
2021-08-09 17:37:55 -0500

    Creación del repositorio.

---
+++ src/BreakingBadDB.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const BreakingBadDB = () => {
+    return (
+        <div>
+            <h1>
+                Hola Mundo
+            </h1>
+        </div>
+    )
+}
