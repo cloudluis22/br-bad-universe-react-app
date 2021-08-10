166729f004ed352757bf6d07659ee1b9005c4c1f
cloudluis22 <cloudluis22@hotmail.com>
2021-08-09 21:46:35 -0500

    Sass instalado.

--- src/BreakingBadDB.js
+++ src/BreakingBadDB.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { AppRouter } from './components/routes/AppRouter';
 
+import './styles/styles.scss';
+
 export const BreakingBadDB = () => {
   return (
     <div>
