a9605d40796753f595962ebc0eea5f5dcae2e5e3
cloudluis22 <cloudluis22@hotmail.com>
2021-08-10 18:36:44 -0500

    Iniciando página de inicio.

--- src/components/screens/MainScreen.js
+++ src/components/screens/MainScreen.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
+import brbaImg from '../../images/brba-main-screen.jpg';
+import bcsImg from '../../images/bcs-main-screen.jpg';
+
 export const MainScreen = () => {
   return (
-    <div>
-      <h1> Main Screen. </h1>
+    <div className='main-background'>
+      <header>
+        <h1 className='title'> The Breaking Bad Universe </h1>
+        <h2 className='subtitle'> Choose a show. </h2>
+      </header>
+
+      <main className='main-container'>
+        <div>
+          <img src={brbaImg} alt='Breaking Bad' />
+          <p> Breaking Bad </p>
+        </div>
+
+        <div>
+          <img src={bcsImg} alt='Better Call Saul' />
+          <p> Better Call Saul </p>
+        </div>
+      </main>
     </div>
   );
 };
