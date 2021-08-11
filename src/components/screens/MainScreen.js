feee0202269c5c9e0b74c39a62299f075d8cc415
cloudluis22 <cloudluis22@hotmail.com>
2021-08-11 13:06:13 -0500

    Implementado el useState para añadir la característica.

--- src/components/screens/MainScreen.js
+++ src/components/screens/MainScreen.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import brbaImg from '../../images/brba-main-screen.jpg';
 import bcsImg from '../../images/bcs-main-screen.jpg';
 
 export const MainScreen = () => {
+  let backgroundClass = '';
+
+  const [currentColor, setCurrentColor] = useState('background-light-green');
+
+  const changeBackground = ({ target }) => {
+    switch (target.alt) {
+      case 'Breaking Bad':
+        backgroundClass = 'background-brba-green';
+        break;
+
+      case 'Better Call Saul':
+        backgroundClass = 'background-bcs-red';
+        break;
+
+      default:
+        backgroundClass = 'background-light-green';
+        break;
+    }
+
+    console.log(backgroundClass);
+  };
+
   return (
-    <div className='main-background'>
+    <div className='background-light-green'>
       <header>
         <h1 className='title'> The Breaking Bad Universe </h1>
         <h2 className='subtitle'> Choose a show. </h2>
@@ -17,7 +39,7 @@ export const MainScreen = () => {
             src={brbaImg}
             alt='Breaking Bad'
             className='main-img'
-            onMouseEnter={() => console.log('hi')}
+            onMouseEnter={changeBackground}
           />
           <p className='show-title'> Breaking Bad </p>
         </div>
@@ -27,7 +49,7 @@ export const MainScreen = () => {
             src={bcsImg}
             alt='Better Call Saul'
             className='main-img'
-            onMouseEnter={() => console.log('hi')}
+            onMouseEnter={changeBackground}
           />
           <p className='show-title'> Better Call Saul </p>
         </div>
