83e57f1507ddbf7fa3fca8d52e5ce22b4fe221bd
cloudluis22 <cloudluis22@hotmail.com>
2021-08-11 20:42:57 -0500

    Font Awesome iconos añadidos.

---
+++ src/components/ui/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+export const Footer = () => {
+  return (
+    <div>
+      <div className='icons-container'></div>
+    </div>
+  );
+};
