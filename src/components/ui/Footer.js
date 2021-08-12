0f248b1423e9652eac5a5b87d55304f1140cd2a7
cloudluis22 <cloudluis22@hotmail.com>
2021-08-11 21:04:04 -0500

    Diseño inicial del footer.

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faCode, faDatabase } from '@fortawesome/free-solid-svg-icons';
 
 export const Footer = () => {
   return (
     <div>
-      <div className='icons-container'></div>
+      <div className='icons-container'>
+        <FontAwesomeIcon icon={faGithub} />
+        <FontAwesomeIcon icon={faCode} />
+        <FontAwesomeIcon icon={faDatabase} />
+      </div>
+      <div className='about-info'>
+        This web application was made using React JS and The Breaking Bad API.
+      </div>
+      <div className='copyright-info'>© created by cloudluis22</div>
     </div>
   );
 };
