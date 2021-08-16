74a31e226bdf6bfaf5cf90f35577546c78ef55b6
cloudluis22 <cloudluis22@hotmail.com>
2021-08-16 13:58:33 -0500

    Diseñando barra de busqueda.

--- src/components/ui/SearchBar.js
+++ src/components/ui/SearchBar.js
@@ -2,16 +2,18 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons';
 
-export const SearchBar = () => {
+export const SearchBar = ({ color }) => {
   return (
     <div className='search-bar-wrapper'>
-      <FontAwesomeIcon icon={faSearch} className='search-icon' />
       <input
         type='text'
-        placeholder='Looking for a character/characters?'
+        placeholder='Search characters'
         className='search-bar'
       />
       <FontAwesomeIcon icon={faTimes} className='clear-icon' />
+      <button type='submit' className='search-btn search-color-green'>
+        <FontAwesomeIcon icon={faSearch} />
+      </button>
     </div>
   );
 };
