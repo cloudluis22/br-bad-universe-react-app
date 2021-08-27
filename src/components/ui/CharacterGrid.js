7937a2ae12eb8b3652e22b2012dc2cee9ad5981f
cloudluis22 <cloudluis22@hotmail.com>
2021-08-27 14:05:38 -0500

    Comenzando desarrollo de busqueda no encontrada.

--- src/components/ui/CharacterGrid.js
+++ src/components/ui/CharacterGrid.js
@@ -2,17 +2,21 @@ import React from 'react';
 import { CharacterCard } from './CharacterCard';
 
 export const CharacterGrid = ({ data }) => {
-  return (
-    <div className='character-grid'>
-      {data.map((char) => {
-        return (
-          <CharacterCard
-            imgSrc={char.img}
-            name={char.name}
-            key={char.char_id}
-          />
-        );
-      })}
-    </div>
-  );
+  if (data.length > 0) {
+    return (
+      <div className='character-grid'>
+        {data.map((char) => {
+          return (
+            <CharacterCard
+              imgSrc={char.img}
+              name={char.name}
+              key={char.char_id}
+            />
+          );
+        })}
+      </div>
+    );
+  } else {
+    return <p>no content</p>;
+  }
 };
