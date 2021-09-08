caf308305fe0f5fd1048c1f25af403a9337ce2ed
cloudluis22 <cloudluis22@hotmail.com>
2021-09-08 11:56:00 -0500

    Añadidos recursos y comenzando elaboración de la página de BCS.

--- src/components/screens/BCSScreen.js
+++ src/components/screens/BCSScreen.js
@@ -1,9 +1,30 @@
 import React from 'react';
 
+import { useFetchData } from '../../hooks/useFetchData';
+
+import bcsFullSizeImg from '../../images/bcs-screen-full.png';
+import bcsMobileSizeImg from '../../images/bcs-screen-mobile.png';
+
 export const BCSScreen = () => {
+  const handleSearch = (value = '') => {
+    setSearch(value);
+  };
+
+  const { dataState, setSearch } = useFetchData(false);
+  const { data, loading, ok } = dataState;
+
   return (
-    <div>
-      <h1> Better Call Saul Screen. </h1>
+    <div className='background-bcs-red'>
+      <img
+        src={bcsFullSizeImg}
+        alt='Better Call Saul Full Logo'
+        className='bcs-logo-full'
+      />
+      <img
+        src={bcsMobileSizeImg}
+        alt='Better Call Saul Mobile Logo'
+        className='bcs-logo-mobile'
+      />
     </div>
   );
 };
--- src/components/screens/BrBaScreen.js
+++ src/components/screens/BrBaScreen.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React from 'react';
 
 import { CharacterModal } from '../characters/CharacterModal';
 import { SearchBar } from '../ui/SearchBar';
